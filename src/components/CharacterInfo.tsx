b9d66aa1b7fe8269c66633b89216484cc84b9d03
Pedro Giampietro <pedrogiampietro@hotmail.com>
2023-10-14 14:49:25 -0300

    fix: interface to props

--- src/components/CharacterInfo.tsx
+++ src/components/CharacterInfo.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
 
-function getMagicTypeColor(magicType: string): string {
-	switch (magicType) {
-		case 'Fire':
-			return '#FF5733'; // Vermelho para Fogo
-		case 'Water':
-			return '#33AFFF'; // Azul para Água
-		case 'Sound':
-			return '#FFD133'; // Amarelo para Som
-		case 'Nature':
-			return '#33FF57'; // Verde para Natureza
-		default:
-			return '#007acc'; // Cor padrão caso não tenha correspondência
-	}
-}
+type Character = {
+	name: string;
+	magicType: 'Fire' | 'Water' | 'Sound' | 'Nature';
+	damage: number;
+	speed: number;
+	accuracy: number;
+	armor: number;
+};
 
-const CharacterInfo: React.FC<{ character: (typeof characters)[0] }> = ({
-	character,
-}) => {
-	const magicTypeColor = getMagicTypeColor(character.magicType);
-
+const CharacterInfo: React.FC<{ character: Character }> = ({ character }) => {
 	return (
 		<div className='character-info-card'>
 			<h2>{character.name}</h2>
